df048029b6d23e077ba1c9f329adc8083ff61922
Prathmesh_Patil <Prathmesh.Patil@xoriantcorp.onmicrosoft.com>
2022-10-06 12:19:58 +0530

    added new files

---
+++ Azure DBs/sample.sql
@@ -0,0 +1,14 @@
+create database Emp;
+use Emp;
+
+create table info(id int, name varchar(255), city varchar(255));
+
+insert into info values(1,"Alex", "Singapore");
+insert into info values(10,"Sid", "Dubai");
+insert into info values(18,"Ravi", "Banglore");
+insert into info values(13,"Tory", "Miami");
+insert into info values(16,"Miguel", "Torento");
+insert into info values(25,"Lenard", "Paris");
+
+
+select * from info;
